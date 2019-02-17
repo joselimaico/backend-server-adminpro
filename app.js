47660b070d47ff6201892b4ec8de32083a8da7f4
jfl96 <joselimaicotele@gmail.com>
2019-02-17 16:45:22 -0500

    usuario y token listo

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 //Requires
 var express = require('express');
 var mongoose = require('mongoose');
-
-
-
+var bodyParser = require('body-parser');
 
 //inicializar variables
 var app = express();
 
+//BodyParser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//importar rutas
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 //conexion base de datos
 
@@ -18,12 +26,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 });
 
 //Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-})
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
 
 
 // escuchar peticiones
---
+++ models/usuario.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var Schema = mongoose.Schema;
+
+var rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol permitido'
+};
+
+var usuarioSchema = new Schema({
+
+    nombre: { type: String, required: [true, 'el nombre es necesario'] },
+    email: { type: String, unique: true, required: [true, 'el correo es necesario'] },
+    password: { type: String, required: [true, 'la contraseña es necesaria'] },
+    img: { type: String, required: false },
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos },
+});
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe ser unico' });
+module.exports = mongoose.model('Usuario', usuarioSchema);
